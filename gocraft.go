767ff886dc13109cb023738643fead0cc88db7af
najeira <najeira@gmail.com>
2015-11-22 17:01:42 +0900

    update gocraft

--- gocraft.go
+++ gocraft.go
@@ -14,7 +14,12 @@ func initGocraft() {
 	var ctx struct{}
 	h := gocraft.New(ctx)
 	h.Get("/", func(w gocraft.ResponseWriter, r *gocraft.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		fmt.Fprintf(w, "Hello, World")
+	})
+	h.Get("/:name", func(w gocraft.ResponseWriter, r *gocraft.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		fmt.Fprintf(w, "Hello, %s", r.PathParams["name"])
 	})
 	registerHandler("gocraft", h)
 }
--- gocraft_test.go
+++ gocraft_test.go
@@ -5,7 +5,16 @@ import (
 	"testing"
 )
 
-func BenchmarkGocraft(b *testing.B) {
+func TestGocraftParam(t *testing.T) {
+	testRequestWithPathParam(t, getHandler("gocraft"))
+}
+
+func BenchmarkGocraftSimple(b *testing.B) {
 	req, _ := http.NewRequest("GET", "/", nil)
+	benchRequest(b, getHandler("gocraft"), req)
+}
+
+func BenchmarkGocraftParam(b *testing.B) {
+	req, _ := http.NewRequest("GET", "/gopher", nil)
 	benchRequest(b, getHandler("gocraft"), req)
 }
