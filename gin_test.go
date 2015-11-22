809fccf409e9e909c8cc8d94439b1735de924cab
najeira <najeira@gmail.com>
2015-11-22 17:02:47 +0900

    fix

--- gin_test.go
+++ gin_test.go
@@ -2,22 +2,11 @@ package main
 
 import (
 	"net/http"
-	"strings"
 	"testing"
 )
 
 func TestGinParam(t *testing.T) {
-	req, _ := http.NewRequest("GET", "/gopher", nil)
-	c, b, h := sendRequest(getHandler("gin"), req)
-	if c != 0 && c != 200 {
-		t.Errorf("invalid status code")
-	}
-	if !strings.Contains(string(b), "gopher") {
-		t.Errorf("invalid body")
-	}
-	if h == nil || !strings.Contains(h["Content-Type"][0], "text/plain") {
-		t.Errorf("invalid header")
-	}
+	testRequestWithPathParam(t, getHandler("gin"))
 }
 
 func BenchmarkGinSimple(b *testing.B) {
