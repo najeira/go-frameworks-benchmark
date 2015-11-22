f87673c677c1c9d96698d87164a231ab099ca9f4
najeira <najeira@gmail.com>
2015-11-22 17:02:11 +0900

    update kami

--- kami_test.go
+++ kami_test.go
@@ -2,22 +2,11 @@ package main
 
 import (
 	"net/http"
-	"strings"
 	"testing"
 )
 
 func TestKami(t *testing.T) {
-	req, _ := http.NewRequest("GET", "/gopher", nil)
-	c, b, h := sendRequest(getHandler("kami"), req)
-	if c != 0 && c != 200 {
-		t.Errorf("invalid status code")
-	}
-	if !strings.Contains(string(b), "gopher") {
-		t.Errorf("invalid body")
-	}
-	if h == nil || !strings.Contains(h["Content-Type"][0], "text/plain") {
-		t.Errorf("invalid header")
-	}
+	testRequestWithPathParam(t, getHandler("kami"))
 }
 
 func BenchmarkKamiSimple(b *testing.B) {
