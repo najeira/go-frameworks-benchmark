046a9ceab6b909db09a775069946672b853fd74b
najeira <najeira@gmail.com>
2020-03-11 19:07:57 +0900

    update: echo v4

--- echo.go
+++ echo.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 
-	"github.com/labstack/echo"
+	"github.com/labstack/echo/v4"
 )
 
 func init() {
@@ -13,9 +13,11 @@ func init() {
 func initEcho() {
 	h := echo.New()
 	h.GET("/", func(c echo.Context) error {
+		// echo writes Content-Type internally
 		return c.String(200, "Hello, World")
 	})
 	h.GET("/:name", func(c echo.Context) error {
+		// echo writes Content-Type internally
 		return c.String(200, fmt.Sprintf("Hello, %s", c.Param("name")))
 	})
 	registerHandler("echo", h)
